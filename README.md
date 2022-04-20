5de984c17ab47b67d57238d0a81a2fad598ecf2e
kudesnik <kudesnik@iMac-kudesnik.local>
2022-04-20 04:47:18 +0300

    done with tablet version

--- README.md
+++ README.md
@@ -1,18 +1,10 @@
 # kaspersky-energizer
 Test task (simple web-page)
 
-Why is it in English? ...i dunno 
 
-Wanring:
 Different font so the end result is not pixel perfect.
 I changed "old prices" because otherwise the maths didn't add up.
 Whats up with second question's height?
 FAQ answers lack soft animation.
-
-
-
-Nobody is perfect.
-
-
-
-Btw, unlike any other pages I saw on one of the pages (https://partnersearch.kaspersky.com/?b2b&locale=en) the Kaspersky logo is not clickable therefore you can't go to the main page through the current "Find a Partner". I know... not my business, but it's just an observation which I'm sharing because there might be a mistake... Wonderful website though!
+FAQ answers go beyond footer.
+Regret doing the task vanilla, should've chosen React. Especially because of the slider which I haven't done.
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 function FAQanswers() {
-  const card = document.querySelectorAll(".faq_main_card");
-  const faqButton = document.querySelectorAll(".faq_main_card_button");
-  const answer = document.querySelectorAll(".faq_main_card_answer");
+  const card = document.querySelectorAll(".faq_main_card"),
+    faqButton = document.querySelectorAll(".faq_main_card_button"),
+    answer = document.querySelectorAll(".faq_main_card_answer");
 
   for (let i = 0; i < 7; i++) {
     card[i].addEventListener("mousedown", () => {
@@ -19,10 +19,10 @@ function FAQanswers() {
 }
 
 function FAQtoggle() {
-  const button = document.querySelector(".faq_radio-button");
-  const header = document.querySelector(".faq_header");
-  const faqMain = document.querySelector(".faq_main");
-  const sysReqMain = document.querySelector(".sys-req_main");
+  const button = document.querySelector(".faq_radio-button"),
+    header = document.querySelector(".faq_header"),
+    faqMain = document.querySelector(".faq_main"),
+    sysReqMain = document.querySelector(".sys-req_main");
   for (let i = 0; i < 2; i++) {
     button.children[i].addEventListener("mousedown", () => {
       if (button.children[i].classList.contains("faq_radio-button-inactive")) {
@@ -54,13 +54,95 @@ function autoRenewal() {
   const checkboxes = document.querySelectorAll(".auto-renewal_checkbox");
   for (let i = 0; i < 3; i++) {
     checkboxes[i].addEventListener("mousedown", () => {
-      checkboxes[i].classList.toggle("checked-checkbox")
-    })
+      checkboxes[i].classList.toggle("checked-checkbox");
+    });
   }
+}
+
+function slider() {
+  const slider = document.querySelector(".offers-container"),
+    slides = Array.from(document.querySelectorAll(".offer-card"));
+
+  let isDragging = false,
+    // startPos = 0,
+    // currentTranslate = 0,
+    // prevTranslate = 0,
+    animationID = 0,
+    currentIndex = 0;
+
+  slides.forEach((slide, index) => {
+    slide.addEventListener("touchstart", touchStart(index));
+    slide.addEventListener("touchend", touchEnd);
+    slide.addEventListener("touchmove", touchMove);
+  });
+
+  window.oncontextmenu = function (e) {
+    console.log(window.innerWidth);
+    if (window.innerWidth < 1280) {
+      e.preventDefault();
+      e.stopPropagation();
+      return false;
+    }
+  };
+
+  function touchStart(index) {
+    return function (event) {
+      currentIndex = index;
+      // startPos = getPositionX(event);
+      isDragging = true;
+
+      animationID = requestAnimationFrame(animation);
+    };
+  }
+
+  function touchEnd() {
+    isDragging = false;
+    cancelAnimationFrame(animationID);
+
+    // const movedBy = currentTranslate - prevTranslate;
+
+    // if (movedBy < -150 && currentIndex < slides.length - 1) currentIndex += 1
+    // if (movedBy > 150 && currentIndex > 0) currentIndex -= 1
+
+    // setPositionByIndex()
+
+    slider.classList.remove("grabbing");
+  }
+
+  function touchMove(event) {
+    // const currentPosition = getPositionX(event);
+    // currentTranslate = prevTranslate + currentPosition - startPos;
+    slider.classList.add("grabbing");
+  }
+
+  // function getPositionX(event) {
+  //   return event.touches[0].clientX;
+  // }
+
+  function animation() {
+    // setSliderPosition()
+    if (isDragging) requestAnimationFrame(animation);
+  }
+
+  // function setSliderPosition() {
+  //   slider.style.transform = `translateX(${currentTranslate}px)`
+  // }
+
+  //как же я сейчас жалею, что начала делать это всё на ванильном js, а не реакте....
+
+  // function setPositionByIndex() {
+
+  //   currentTranslate = currentIndex * 174
+  //   prevTranslate = currentTranslate
+  //   setSliderPosition()
+  // }
+
+
 }
 
 window.addEventListener("load", () => {
   FAQanswers();
   FAQtoggle();
   autoRenewal();
+  slider();
 });
